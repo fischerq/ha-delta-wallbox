3fa62b731dc2856d4d756b5da2ba5b8cc1799302
quirin.niedernhuber <quirin.niedernhuber@gmail.com>
2025-10-14 21:06:41 +0000

    only read 2

--- custom_components/delta_wallbox/coordinator.py
+++ custom_components/delta_wallbox/coordinator.py
@@ -96,9 +96,10 @@ class DeltaWallboxDataUpdateCoordinator(DataUpdateCoordinator):
         regs = await self._read_register(register, 2)
         return self._decode_f32(regs) if regs else 0.0
 
-    async def _read_string(self, register, count):
+    async def _read_string(self, register, max_count):
         """Read a string from a number of registers."""
-        regs = await self._read_register(register, count)
+        regs = await self._read_register(register, 1)
+        # TODO: read up to max_count registers, stop when a register is 0
         return "".join([chr(c) for c in regs if c != 0]) if regs else ""
 
     async def _async_update_data(self):
@@ -113,60 +114,60 @@ class DeltaWallboxDataUpdateCoordinator(DataUpdateCoordinator):
             # Charger-Level Sensors
             data["charger_state"] = await self._read_u16(REG_CHARGER_STATE)
             _LOGGER.debug(f"Charger state: {data['charger_state']}")
-            data["evse_count"] = await self._read_u16(REG_EVSE_COUNT)
-            _LOGGER.debug(f"EVSE count: {data['evse_count']}")
-            data["serial_number"] = await self._read_string(REG_SERIAL_NUMBER, 20)
-            _LOGGER.debug(f"Serial number: {data['serial_number']}")
+#            data["evse_count"] = await self._read_u16(REG_EVSE_COUNT)
+#            _LOGGER.debug(f"EVSE count: {data['evse_count']}")
+#            data["serial_number"] = await self._read_string(REG_SERIAL_NUMBER, 20)
+#            _LOGGER.debug(f"Serial number: {data['serial_number']}")
             data["model"] = await self._read_string(REG_MODEL, 20)
             _LOGGER.debug(f"Model: {data['model']}")
 
-            # EVSE-Level Sensors
-            data["evse_state"] = await self._read_u16(REG_EVSE_STATE)
-            _LOGGER.debug(f"EVSE state: {data['evse_state']}")
-            data["ev_connected"] = await self._read_u16(REG_EV_CONNECTED)
-            _LOGGER.debug(f"EV connected: {data['ev_connected']}")
-            data["charging_time"] = await self._read_u32(REG_CHARGING_TIME)
-            _LOGGER.debug(f"Charging time: {data['charging_time']}")
-            data["charging_power"] = await self._read_u32(REG_CHARGING_POWER)
-            _LOGGER.debug(f"Charging power: {data['charging_power']}")
-            data["charged_energy"] = await self._read_u32(REG_CHARGED_ENERGY)
-            _LOGGER.debug(f"Charged energy: {data['charged_energy']}")
+#            # EVSE-Level Sensors
+#            data["evse_state"] = await self._read_u16(REG_EVSE_STATE)
+#            _LOGGER.debug(f"EVSE state: {data['evse_state']}")
+#            data["ev_connected"] = await self._read_u16(REG_EV_CONNECTED)
+#            _LOGGER.debug(f"EV connected: {data['ev_connected']}")
+#            data["charging_time"] = await self._read_u32(REG_CHARGING_TIME)
+#            _LOGGER.debug(f"Charging time: {data['charging_time']}")
+#            data["charging_power"] = await self._read_u32(REG_CHARGING_POWER)
+#            _LOGGER.debug(f"Charging power: {data['charging_power']}")
+#            data["charged_energy"] = await self._read_u32(REG_CHARGED_ENERGY)
+#            _LOGGER.debug(f"Charged energy: {data['charged_energy']}")
 
-            soc_raw = await self._read_u16(REG_SOC)
-            data["soc"] = soc_raw / 10.0
-            _LOGGER.debug(f"SOC: {data['soc']}")
+#            soc_raw = await self._read_u16(REG_SOC)
+#            data["soc"] = soc_raw / 10.0
+#            _LOGGER.debug(f"SOC: {data['soc']}")
 
-            data["ev_max_power"] = await self._read_u32(REG_EV_MAX_POWER)
-            _LOGGER.debug(f"EV max power: {data['ev_max_power']}")
-            data["ev_min_power"] = await self._read_u32(REG_EV_MIN_POWER)
-            _LOGGER.debug(f"EV min power: {data['ev_min_power']}")
-            data["p1_voltage"] = await self._read_u32(REG_P1_VOLTAGE)
-            _LOGGER.debug(f"P1 voltage: {data['p1_voltage']}")
-            data["p2_voltage"] = await self._read_u32(REG_P2_VOLTAGE)
-            _LOGGER.debug(f"P2 voltage: {data['p2_voltage']}")
-            data["p3_voltage"] = await self._read_u32(REG_P3_VOLTAGE)
-            _LOGGER.debug(f"P3 voltage: {data['p3_voltage']}")
-            data["p1_current"] = await self._read_u32(REG_P1_CURRENT)
-            _LOGGER.debug(f"P1 current: {data['p1_current']}")
-            data["p2_current"] = await self._read_u32(REG_P2_CURRENT)
-            _LOGGER.debug(f"P2 current: {data['p2_current']}")
-            data["p3_current"] = await self._read_u32(REG_P3_CURRENT)
-            _LOGGER.debug(f"P3 current: {data['p3_current']}")
+#            data["ev_max_power"] = await self._read_u32(REG_EV_MAX_POWER)
+#            _LOGGER.debug(f"EV max power: {data['ev_max_power']}")
+#            data["ev_min_power"] = await self._read_u32(REG_EV_MIN_POWER)
+#            _LOGGER.debug(f"EV min power: {data['ev_min_power']}")
+ #           data["p1_voltage"] = await self._read_u32(REG_P1_VOLTAGE)
+ #           _LOGGER.debug(f"P1 voltage: {data['p1_voltage']}")
+ #           data["p2_voltage"] = await self._read_u32(REG_P2_VOLTAGE)
+ #           _LOGGER.debug(f"P2 voltage: {data['p2_voltage']}")
+ #           data["p3_voltage"] = await self._read_u32(REG_P3_VOLTAGE)
+ #           _LOGGER.debug(f"P3 voltage: {data['p3_voltage']}")
+ #           data["p1_current"] = await self._read_u32(REG_P1_CURRENT)
+ #           _LOGGER.debug(f"P1 current: {data['p1_current']}")
+ #           data["p2_current"] = await self._read_u32(REG_P2_CURRENT)
+ #           _LOGGER.debug(f"P2 current: {data['p2_current']}")
+  #          data["p3_current"] = await self._read_u32(REG_P3_CURRENT)
+  #          _LOGGER.debug(f"P3 current: {data['p3_current']}")
 
-            grid_freq_raw = await self._read_u16(REG_GRID_FREQUENCY)
-            data["grid_frequency"] = grid_freq_raw / 100.0
-            _LOGGER.debug(f"Grid frequency: {data['grid_frequency']}")
+ #           grid_freq_raw = await self._read_u16(REG_GRID_FREQUENCY)
+ #           data["grid_frequency"] = grid_freq_raw / 100.0
+ #           _LOGGER.debug(f"Grid frequency: {data['grid_frequency']}")
 
-            data["grid_total_power"] = await self._read_f32(REG_GRID_TOTAL_POWER)
-            _LOGGER.debug(f"Grid total power: {data['grid_total_power']}")
-            data["grid_p1_power"] = await self._read_f32(REG_GRID_P1_POWER)
-            _LOGGER.debug(f"Grid P1 power: {data['grid_p1_power']}")
-            data["grid_p2_power"] = await self._read_f32(REG_GRID_P2_POWER)
-            _LOGGER.debug(f"Grid P2 power: {data['grid_p2_power']}")
-            data["grid_p3_power"] = await self._read_f32(REG_GRID_P3_POWER)
-            _LOGGER.debug(f"Grid P3 power: {data['grid_p3_power']}")
-            data["error_code"] = await self._read_u64(REG_ERROR_CODE)
-            _LOGGER.debug(f"Error code: {data['error_code']}")
+#            data["grid_total_power"] = await self._read_f32(REG_GRID_TOTAL_POWER)
+#            _LOGGER.debug(f"Grid total power: {data['grid_total_power']}")
+ #           data["grid_p1_power"] = await self._read_f32(REG_GRID_P1_POWER)
+ #           _LOGGER.debug(f"Grid P1 power: {data['grid_p1_power']}")
+ #           data["grid_p2_power"] = await self._read_f32(REG_GRID_P2_POWER)
+ #           _LOGGER.debug(f"Grid P2 power: {data['grid_p2_power']}")
+ #           data["grid_p3_power"] = await self._read_f32(REG_GRID_P3_POWER)
+ #           _LOGGER.debug(f"Grid P3 power: {data['grid_p3_power']}")
+ #           data["error_code"] = await self._read_u64(REG_ERROR_CODE)
+ #           _LOGGER.debug(f"Error code: {data['error_code']}")
 
             _LOGGER.debug(f"Returning data: {data}")
             return data
